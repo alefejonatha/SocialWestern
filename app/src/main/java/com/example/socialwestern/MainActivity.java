511c72b6a77a6cded07ec1dfdcc4362376c284ff
alefejonatha <60615421+alefejonatha@users.noreply.github.com>
2021-09-20 19:41:44 -0300

    commit toolBar

--- app/src/main/java/com/example/socialwestern/MainActivity.java
+++ app/src/main/java/com/example/socialwestern/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.socialwestern;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +32,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TabLayout tabLayout = findViewById(R.id.top_nav);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_home));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_forum_24));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_people_24));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_tvplay));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_notifications));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_hamb));
+
+
+
         RecyclerView rv = findViewById(R.id.recycler_view);
         rv.setLayoutManager(new LinearLayoutManager(this));
         postAdapter = new PostAdapter();
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rv.getContext(), DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(ContextCompat.getDrawable(this,R.drawable.divider));
+        rv.addItemDecoration(dividerItemDecoration);
+
         rv.setAdapter(postAdapter);
 
         List<Post> posts = new ArrayList<>();
@@ -39,8 +62,29 @@ public class MainActivity extends AppCompatActivity {
         post1.setTextViewTitle("interior.com".toUpperCase(Locale.ROOT));
         post1.setTextViewSubtitle("Assim mesmo, o fenômeno da Internet pode nos levar a considerar a reestruturação dos procedimentos normalmente adotados.");
         post1.setTextViewTime("2 min");
+        posts.add(post1);
+
+        Post post2 = new Post();
+        post2.setImageViewUser(R.drawable.gato);
+        post2.setImageViewPost(R.drawable.teclado);
+        post2.setTextViewUsername("Cat");
+        post2.setTextViewContent("Podemos já vislumbrar o modo pelo qual o fenômeno da Internet desafia a capacidade de equalização dos conhecimentos estratégicos para atingir a excelência. o mundo atual, a competitividade nas transações comerciais deve passar por modificações independentemente do impacto na agilidade decisória.");
+        post2.setTextViewTime("2 min");
+        posts.add(post2);
 
         posts.add(post1);
+        posts.add(post2);
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post1);
+        posts.add(post2);
+
 
         postAdapter.setPosts(posts);
         postAdapter.notifyDataSetChanged();
@@ -76,6 +120,20 @@ public class MainActivity extends AppCompatActivity {
             textViewContent.setText(post.getTextViewContent());
             textViewTitle.setText(post.getTextViewTitle());
             textViewSubtitle.setText(post.getTextViewSubtitle());
+
+            if (post.getTextViewTitle() == null){
+                itemView.findViewById(R.id.post_container).setVisibility(View.GONE);
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone((ConstraintLayout) itemView);
+                constraintSet.setDimensionRatio(R.id.image_view_post, "1:1");
+                constraintSet.applyTo((ConstraintLayout) itemView);
+            }else {
+                itemView.findViewById(R.id.post_container).setVisibility(View.VISIBLE);
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone((ConstraintLayout) itemView);
+                constraintSet.setDimensionRatio(R.id.image_view_post, "16:9");
+                constraintSet.applyTo((ConstraintLayout) itemView);
+            }
         }
     }
 
